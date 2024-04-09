765751ca2abb183ff71fc9ea71e9f2de6999dc57
AnnaKharatova <black-isida@yandex.ru>
2024-04-10 02:38:17 +0300

    init

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import { postsArray } from '../scripts/constants.js'
+
+const container = document.getElementById('container');
+
+postsArray.forEach((item, index) => {
+    const element = document.createElement('li');
+    element.classList.add('posts__item');
+
+    const title = document.createElement('h2');
+    title.classList.add('posts__title');
+    title.textContent = item.title;
+
+    const text = document.createElement('p');
+    text.classList.add('posts__text');
+    text.textContent = item.text;
+
+    element.appendChild(title);
+    element.appendChild(text);
+
+    container.appendChild(element);
+});
+
+const elements = document.querySelectorAll(".posts__item");
+
+document.addEventListener('click', function () {
+    const floating = document.getElementById('floating')
+    floating.classList.add('floating-element__clicked');
+});
+
+elements.forEach(el => {
+    el.addEventListener('click', function () {
+        el.classList.add('posts__item_clicked');
+    });
+})
+
